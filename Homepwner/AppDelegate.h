91e55c4ec5f0c57658fc7e72d6bdb6e5a5b5c50a
Shulin <wangwangwar@gmail.com>
2014-10-26 11:04:00 +0800

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by wangwangwar on 14/10/26.
+//  Copyright (c) 2014年 wangwangwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homepwner
+//
+//  Created by wangwangwar on 14/10/26.
+//  Copyright (c) 2014年 wangwangwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
