acf22842b33c6852df986f5212190cbbc6c40a98
Shulin <wangwangwar@gmail.com>
2014-10-27 20:46:23 +0800

    Add Table View Cell

--- Homepwner/BNRItemViewController.m
+++ Homepwner/BNRItemViewController.m
@@ -7,16 +7,37 @@
 //
 
 #import "BNRItemViewController.h"
+#import "BNRItemStore.h"
+#import "BNRItem.h"
 
 @implementation BNRItemViewController
 
 - (instancetype)init {
     self = [super initWithStyle:UITableViewStylePlain];
+    if (self) {
+        for (int i = 0; i < 5; i++) {
+            [[BNRItemStore sharedStore] createItem];
+        }
+    }
     return self;
 }
 
 - (instancetype)initWithStyle:(UITableViewStyle)style {
     return [self init];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UITableViewCell"];
+    NSArray *items = [[BNRItemStore sharedStore] allItems];
+    BNRItem *item = items[indexPath.row];
+    
+    cell.textLabel.text = [item description];
+    
+    return cell;
 }
 
 @end
