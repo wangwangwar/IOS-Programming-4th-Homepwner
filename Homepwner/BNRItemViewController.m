a10f43cfe1622b6c0b5a0cf10a8919b04ded61ae
Shulin <wangwangwar@gmail.com>
2014-10-27 21:00:05 +0800

    Reuse UITableViewCell If We Can

--- Homepwner/BNRItemViewController.m
+++ Homepwner/BNRItemViewController.m
@@ -31,13 +31,18 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UITableViewCell"];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
     NSArray *items = [[BNRItemStore sharedStore] allItems];
     BNRItem *item = items[indexPath.row];
     
     cell.textLabel.text = [item description];
     
     return cell;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
 }
 
 @end
