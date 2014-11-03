e3749e3e6507f19c944b07ced897f95545dc7b70
Shulin <wangwangwar@gmail.com>
2014-11-03 13:44:23 +0800

    Chapter8 - Bronze Challenge: Sections

--- Homepwner/BNRItemViewController.m
+++ Homepwner/BNRItemViewController.m
@@ -10,6 +10,13 @@
 #import "BNRItemStore.h"
 #import "BNRItem.h"
 
+@interface BNRItemViewController()
+
+@property (nonatomic) NSMutableArray *itemsWorthMoreThan50;
+@property (nonatomic) NSMutableArray *itemsOther;
+
+@end
+
 @implementation BNRItemViewController
 
 - (instancetype)init {
@@ -19,6 +26,22 @@
             [[BNRItemStore sharedStore] createItem];
         }
     }
+    
+    NSArray *allItems = [[BNRItemStore sharedStore] allItems];
+    self.itemsWorthMoreThan50 = [NSMutableArray new];
+    self.itemsOther = [NSMutableArray new];
+    
+    // Separate them into ">50" and "other"
+    for (int i = 0; i < [allItems count]; i++) {
+        BNRItem *item = [allItems objectAtIndex:i];
+        NSLog(@"%@ %d", item, [item valueInDollars]);
+        if ([item valueInDollars] > 50) {
+            [self.itemsWorthMoreThan50 addObject:item];
+        } else {
+            [self.itemsOther addObject:item];
+        }
+    }
+    
     return self;
 }
 
@@ -27,17 +50,41 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [[[BNRItemStore sharedStore] allItems] count];
+    if (section == 0) {
+        return [self.itemsWorthMoreThan50 count];
+    } else {
+        return [self.itemsOther count];
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
-    NSArray *items = [[BNRItemStore sharedStore] allItems];
-    BNRItem *item = items[indexPath.row];
+    BNRItem *item;
+    if (indexPath.section == 0) {
+        item = self.itemsWorthMoreThan50[indexPath.row];
+    } else {
+        item = self.itemsOther[indexPath.row];
+    }
     
     cell.textLabel.text = [item description];
     
     return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
+    return @[@">50", @"other"];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        return @">50";
+    } else {
+        return @"other";
+    }
 }
 
 - (void)viewDidLoad {
