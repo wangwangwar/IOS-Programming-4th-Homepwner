1c938cd5c02e3e29b692b4b42ef05b0f262d2d3f
Shulin <wangwangwar@gmail.com>
2014-11-04 13:52:58 +0800

    Remove Initialization of 5 Item

--- Homepwner/BNRItemViewController.m
+++ Homepwner/BNRItemViewController.m
@@ -20,11 +20,6 @@
 
 - (instancetype)init {
     self = [super initWithStyle:UITableViewStylePlain];
-    if (self) {
-        for (int i = 0; i < 5; i++) {
-            [[BNRItemStore sharedStore] createItem];
-        }
-    }
     return self;
 }
 
