6f3d05f90abd47279a88f75b8959666c19fe2c15
Shulin <wangwangwar@gmail.com>
2014-11-06 14:52:29 +0800

    Add BNRDetailViewController with its XIB for Viewing Detail

---
+++ Homepwner/BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by wangwangwar on 14/11/6.
+//  Copyright (c) 2014年 wangwangwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
---
+++ Homepwner/BNRDetailViewController.m
@@ -0,0 +1,22 @@
+//
+//  BNRDetailViewController.m
+//  Homepwner
+//
+//  Created by wangwangwar on 14/11/6.
+//  Copyright (c) 2014年 wangwangwar. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+
+@interface BNRDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *SerialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *DateLabel;
+
+@end
+
+@implementation BNRDetailViewController
+
+@end
