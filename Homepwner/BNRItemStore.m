cea9a514a4d1eee8dd0fe2eb41273b3e10a9fb31
Shulin <wangwangwar@gmail.com>
2014-11-19 18:58:27 +0800

    Make BNRItemStore Thread-Safe

--- Homepwner/BNRItemStore.m
+++ Homepwner/BNRItemStore.m
@@ -67,9 +67,11 @@
 + (instancetype)sharedStore {
     static BNRItemStore *sharedStore = nil;
     
-    if (!sharedStore) {
+    // Thread safe
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
         sharedStore = [[self alloc] initPrivate];
-    }
+    });
     
     return sharedStore;
 }
