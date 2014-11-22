864d4a80308f6b4266e08b19344c3116962ba680
Shulin <wangwangwar@gmail.com>
2014-11-22 11:24:36 +0800

    Chapter 19 - Subclassing UITableViewCell: Add BNRItemCell Xib, .h and .m File

---
+++ Homepwner/BNRItemCell.h
@@ -0,0 +1,18 @@
+//
+//  BNRItemCell.h
+//  Homepwner
+//
+//  Created by wangwangwar on 14/11/22.
+//  Copyright (c) 2014年 wangwangwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRItemCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *serialNumberLabel;
+@property (weak, nonatomic) IBOutlet UILabel *valueLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *thumbnailView;
+
+@end
---
+++ Homepwner/BNRItemCell.m
@@ -0,0 +1,13 @@
+//
+//  BNRItemCell.m
+//  Homepwner
+//
+//  Created by wangwangwar on 14/11/22.
+//  Copyright (c) 2014年 wangwangwar. All rights reserved.
+//
+
+#import "BNRItemCell.h"
+
+@implementation BNRItemCell
+
+@end
