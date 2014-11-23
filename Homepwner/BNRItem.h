d9c16fc6cef72bb7b5b9dffc6508a989581e3d89
Shulin <wangwangwar@gmail.com>
2014-11-23 22:43:11 +0800

    Update BNRItem Class

--- Homepwner/BNRItem.h
+++ Homepwner/BNRItem.h
@@ -2,28 +2,26 @@
 //  BNRItem.h
 //  Homepwner
 //
-//  Created by wangwangwar on 14/10/27.
+//  Created by wangwangwar on 14/11/23.
 //  Copyright (c) 2014年 wangwangwar. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 #import <UIKit/UIKit.h>
 
-@interface BNRItem : NSObject <NSCoding>
 
-@property (nonatomic, copy) NSString *itemName;
-@property (nonatomic, copy) NSString *serialName;
+@interface BNRItem : NSManagedObject
+
+@property (nonatomic, strong) NSString *itemName;
+@property (nonatomic, strong) NSString *serialNumber;
 @property (nonatomic) int valueInDollars;
-@property (nonatomic, readonly, strong) NSDate *dateCreated;
-@property (nonatomic, copy) NSString *itemKey;
+@property (nonatomic, strong) NSDate *dateCreated;
+@property (nonatomic, strong) NSString *itemKey;
 @property (nonatomic, strong) UIImage *thumbnail;
+@property (nonatomic) double orderingValue;
+@property (nonatomic, strong) NSManagedObject *assetType;
 
-- (instancetype)initWithItemName:(NSString *)name
-                  valueInDollars:(int)value
-                    serialNumber:(NSString *)serialNumber;
-- (instancetype)initWithItemName:(NSString *)name;
 - (void)setThumbnailFromImage:(UIImage *)image;
-+ (instancetype)randomItem;
-
 
 @end
--- Homepwner/BNRItem.m
+++ Homepwner/BNRItem.m
@@ -2,54 +2,23 @@
 //  BNRItem.m
 //  Homepwner
 //
-//  Created by wangwangwar on 14/10/27.
+//  Created by wangwangwar on 14/11/23.
 //  Copyright (c) 2014年 wangwangwar. All rights reserved.
 //
 
 #import "BNRItem.h"
 
+
 @implementation BNRItem
 
-#pragma mark - Initialization
-
-- (instancetype)initWithItemName:(NSString *)name
-                  valueInDollars:(int)value
-                    serialNumber:(NSString *)serialNumber {
-    self = [super init];
-    if (self) {
-        _itemName = name;
-        _serialName = serialNumber;
-        _valueInDollars = value;
-        _dateCreated = [NSDate new];
-        
-        // Create an NSUUID object and get its string representation
-        NSUUID *uuid = [NSUUID new];
-        _itemKey = [uuid UUIDString];
-    }
-    
-    return self;
-}
-
-- (instancetype)initWithItemName:(NSString *)name {
-    self = [super init];
-    if (self) {
-        _itemName = name;
-    }
-    
-    return self;
-}
-
-#pragma mark - Other
-
-- (NSString *)description {
-    NSString *descriptionString =
-    [[NSString alloc] initWithFormat:@"%@ (%@): Worth $%d, recorded on %@",
-     self.itemName,
-     self.serialName,
-     self.valueInDollars,
-     self.dateCreated];
-    return descriptionString;
-}
+@dynamic itemName;
+@dynamic serialNumber;
+@dynamic valueInDollars;
+@dynamic dateCreated;
+@dynamic itemKey;
+@dynamic thumbnail;
+@dynamic orderingValue;
+@dynamic assetType;
 
 - (void)setThumbnailFromImage:(UIImage *)image {
     CGSize originalImageSize = image.size;
@@ -89,53 +58,14 @@
     UIGraphicsEndImageContext();
 }
 
-#pragma mark - Class Methods
-
-+ (instancetype)randomItem {
-    NSArray *randomAdjectiveList = @[@"Fluffy", @"Rusty", @"Shiny"];
-    NSArray *randomNounList = @[@"Bear", @"Spork", @"Mac"];
+- (void)awakeFromInsert {
+    [super awakeFromInsert];
     
-    NSInteger adjectiveIndex = arc4random() % [randomAdjectiveList count];
-    NSInteger nounIndex = arc4random() % [randomNounList count];
+    self.dateCreated = [NSDate date];
     
-    NSString *randomName = [[NSString alloc] initWithFormat:@"%@ %@", [randomAdjectiveList objectAtIndex:adjectiveIndex], [randomNounList objectAtIndex:nounIndex]];
-    
-    int randomValue = arc4random() % 100;
-    
-    NSString *randomSerialNumber = [[NSString alloc] initWithFormat:@"%c%c%c%c%c",
-                                    'A' + arc4random() % 26,
-                                    '0' + arc4random() % 10,
-                                    'A' + arc4random() % 26,
-                                    '0' + arc4random() % 10,
-                                    'A' + arc4random() % 26];
-    
-    BNRItem *item = [[BNRItem alloc] initWithItemName:randomName valueInDollars:randomValue serialNumber:randomSerialNumber];
-    
-    return item;
-}
-
-#pragma mark - NSCoding Protocol
-
-- (void)encodeWithCoder:(NSCoder *)aCoder {
-    [aCoder encodeObject:self.itemName forKey:@"itemName"];
-    [aCoder encodeObject:self.serialName forKey:@"serialName"];
-    [aCoder encodeObject:self.dateCreated forKey:@"dateCreated"];
-    [aCoder encodeObject:self.itemKey forKey:@"itemKey"];
-    [aCoder encodeObject:self.thumbnail forKey:@"thumbnail"];
-    [aCoder encodeInt:self.valueInDollars forKey:@"valueInDollars"];
-}
-
-- (id)initWithCoder:(NSCoder *)aDecoder {
-    self = [super init];
-    if (self) {
-        _itemName = [aDecoder decodeObjectForKey:@"itemName"];
-        _serialName = [aDecoder decodeObjectForKey:@"serialName"];
-        _dateCreated = [aDecoder decodeObjectForKey:@"dateCreated"];
-        _itemKey = [aDecoder decodeObjectForKey:@"itemKey"];
-        _thumbnail = [aDecoder decodeObjectForKey:@"thumbnail"];
-        _valueInDollars = [aDecoder decodeIntForKey:@"valueInDollars"];
-    }
-    return self;
+    // Create an NSUUID object and get its string representation
+    NSUUID *uuid = [NSUUID new];
+    self.itemKey = [uuid UUIDString];
 }
 
 @end
